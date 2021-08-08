164fdd935c1d04c8e1158f5ad35f8fad38480a46
infadwh <87582000+infadwh@users.noreply.github.com>
2021-08-08 17:32:17 +0530

    Update create_infra.sh

--- Dev/create_infra.sh
+++ Dev/create_infra.sh
@@ -1,1 +1,29 @@
-sh
+if [[ $Create_Infra == true ]]
+then
+	export action="apply"
+	export process="Creating"
+	elif [[ $Delete_Infra == true ]]
+	then
+	export action="destroy"
+	export process="Destroying"
+	elif [[ $Trigger_Code_Simulator == true ]]
+	then
+	export action="apply"
+	else
+		echo "Select either of Create_Infra or Delete_Infra"
+		exit 1
+fi
+
+if [[ $ComputeEngine == true ]]
+then
+	cd ${WORKSPACE}/Dev
+	echo "########### $process ComputeEngine for $project in $env environment ###########"
+	cd ComputeEngine
+	#cp -rf ../provider.tf .
+	rm -rf .terraform
+	terraform init
+	terraform $action -auto-approve
+	#terraform destroy -auto-approve
+	set -xe
+	echo "########## Compute Engine $process is complete ###########"
+fi
