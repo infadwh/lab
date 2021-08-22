3297369d22263b908a48ea815af6cb306c5d297b
infadwh <87582000+infadwh@users.noreply.github.com>
2021-08-22 16:32:34 +0530

    Create main.tf

---
+++ Dev/ComputeEngine/main.tf
@@ -0,0 +1,11 @@
+module "compute-instance" {
+	project_id             	= var.project_id
+	source                  = "../modules"
+	vm_name 				= var.vm_name
+	machine_type	 		= var.machine_type
+	zone 					= var.zone
+	image 					= var.image
+	//interface 				= var.interface
+	network 				= var.network
+    sub_network             = var.sub_network
+}
