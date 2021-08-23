f21bf9c86cacbe98820b0e90ff0443188afc26d0
infadwh <87582000+infadwh@users.noreply.github.com>
2021-08-23 16:06:24 +0530

    Update main.tf

--- Dev/ComputeEngine/main.tf
+++ Dev/ComputeEngine/main.tf
@@ -1,11 +1,10 @@
 module "compute-instance" {
-	project_id             	= var.project_id
-	source                  = "../../modules"
-	vm_name 				= var.vm_name
-	machine_type	 		= var.machine_type
-	zone 					= var.zone
-	image 					= var.image
-	//interface 				= var.interface
-	network 				= var.network
-    sub_network             = var.sub_network
+	source       = "../../modules"
+	project_id   = var.project_id
+	vm_name 	 = var.vm_name
+	machine_type = var.machine_type
+	zone 		 = var.zone
+	image 		 = var.image
+	network 	 = var.network
+        sub_network  = var.sub_network
 }
